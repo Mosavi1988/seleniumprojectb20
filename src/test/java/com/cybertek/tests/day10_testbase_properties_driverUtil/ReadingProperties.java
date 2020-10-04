b121d34fa7980c189f0946ba0e421389cea8f02e
Mosavi1988 <zh_mosavi@hotmail.com>
2020-10-04 14:14:57 -0400

    Reading Properties

---
+++ src/test/java/com/cybertek/tests/day10_testbase_properties_driverUtil/ReadingProperties.java
@@ -0,0 +1,16 @@
+package com.cybertek.tests.day10_testbase_properties_driverUtil;
+
+import org.testng.annotations.Test;
+
+public class ReadingProperties {
+
+    @Test
+    public void reading_from_properties_file(){
+
+        // properties: use properties class object
+        // FileInputStream: opens the file
+        // We will load the file
+        // We read from the file
+        // Close the file
+    }
+}
